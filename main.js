12127897235272bea2a3f6cab2e04532842ce9b7
Praneet0212 <71916363+Praneet0212@users.noreply.github.com>
2021-06-17 18:33:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+noseX=0;
+noseY=0
+
 function preload()
 {
+    clown_nose = loadImage('https://i.postimg.cc/3NnP8FK7/circle-cropped.png');
 }
 
 function setup()
@@ -24,6 +28,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x - 10;
+        noseY = results[0].pose.nose.y - 10;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
@@ -31,7 +37,11 @@ function gotPoses(results)
 
 function draw()
 {
-    image(video, 0, 0, 300, 300); 
+    image(video, 0, 0, 300, 300);
+    //fill(255, 0, 0);
+    //stroke(255, 0, 0); 
+    //circle(noseX, noseY, 20);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot()
